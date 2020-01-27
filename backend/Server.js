9d1e9d8e8b41a9516ede6a5b12d82a0421d4c549
amarpreet-s <amarpreet-s@hcl.com>
2020-01-27 22:56:48 +0530

    second

--- backend/Server.js
+++ backend/Server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 var cors = require('cors');
 const bodyParser=require('body-parser');
-const logger = require('morgon');
+const logger = require('morgan');
 const Data = require('./data');
 
 const PORT = 3001;
@@ -11,16 +11,68 @@ app.use(cors());
 const router = express.Router();
 
 //mongodb setup
-const dbRoute = '';
+const dbRoute = 'mongodb://fullstack:Amarpreet96@ds125372.mlab.com:25372/amar_mongo';
 mongoose.connect(dbRoute, { useNewUrlParser: true});
 
 let db = mongoose.connection;
 //event based connection
-db.once('open',()=>{console.log("connected")});
+db.once('open',()=>{console.log("connected to database")});
 db.on('error',console.error.bind(console,'MongoDB connection error :'));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(logger('dev'));
+
+router.get('/welcome',(req,res)=>{
+        res.json({ success: true });
+});
+
+router.get('/getData',(req,res)=>{
+    Data.find((err,data)=>{
+        if(err) return res.send({success: false, error:err})
+        return res.send({success:true,data:data})
+    })
+});
+
+router.post('/updateData',(req,res)=>{
+    const {id,update} = req.body;
+    Data.findByIdAndUpdate(id,update,{new: true},(err,data)=>{
+        if(err) return res.send({success: false, error:err})
+        return res.json({ success: true, model : data}); 
+    });
+});
+
+router.delete('/deleteData', (req, res) => {
+    const { id } = req.body;
+    Data.findByIdAndRemove(id, (err) => {
+      if (err) return res.send(err);
+      return res.json({ success: true });
+    });
+  });
+
+ router.post('/putData',(req,res)=>{
+    let data = new Data();
+    const {id,message} = req.body;
+    if ((!id && id !== 0) || !message) {
+        return res.json({
+          success: false,
+          error: 'INVALID INPUTS',
+        });
+      }
+    data.id = id;
+    data.message = message;
+    data.save((err)=>{
+        if(err) return res.send({success:false, error :err});
+        return res.send({success:true})
+
+    })
+ });
+
+  
+
+// append /api for our http requests
+app.use('/api', router);
+
+app.listen(PORT,()=>{console.log(`http://localhost:${PORT}/api/welcome`)});
 
 //https://medium.com/javascript-in-plain-english/full-stack-mongodb-react-node-js-express-js-in-one-simple-app-6cc8ed6de274
--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,88 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+
+	state = {
+				data: [],
+				id:0,
+				message: null,
+				intervalIsSet: false,
+				idToDelete: null,
+				idToUpdate: null,
+				objectToUpdate: null
+	};
+
+	ComponentDidMount() {
+		this.getDataFromDb();
+		// !false = true will work for true only
+		if(!this.state.intervalIsSet) {
+			let interval = setInterval(this.getDataFromDb,1000);
+			this.setState({intervalIsSet: interval});
+		}
+	}
+
+	componentWillUnmount() {
+    if (this.state.intervalIsSet) {
+      clearInterval(this.state.intervalIsSet);
+      this.setState({ intervalIsSet: null });
+    }
+  }
+
+  getDataFromDb = () => {
+    fetch('http://localhost:3001/api/getData')
+      .then((data) => data.json())
+      .then((res) => this.setState({ data: res.data }));
+  };
+
+  //calling backend api to put data in DB
+  putDataToDB = (message) => {
+  	let currentIds = this.state.data.map((data)=>{data.id});
+  	let idtoBeAdded = 0;
+  	while(currentIds.includes(idtoBeAdded)){
+  		++idtoBeAdded;
+  	}
+  
+
+  axios.post('http://localhost:3001/api/putData', {
+
+      id: idToBeAdded,
+      message: message
+    });
+ };
+
+//
+ deleteFromDB = (idTodelete) => {
+    parseInt(idTodelete);
+    let objIdToDelete = null;
+    this.state.data.forEach((dat) => {
+      if (dat.id == idTodelete) {
+        objIdToDelete = dat._id;
+      }
+    });
+
+    axios.delete('http://localhost:3001/api/deleteData', {
+      data: {
+        id: objIdToDelete,
+      },
+    });
+  };
+ //
+  updateDB = (idToUpdate, updateToApply) => {
+    let objIdToUpdate = null;
+    parseInt(idToUpdate);
+    this.state.data.forEach((dat) => {
+      if (dat.id == idToUpdate) {
+        objIdToUpdate = dat._id;
+      }
+    });
+
+    axios.post('http://localhost:3001/api/updateData', {
+      id: objIdToUpdate,
+      update: { message: updateToApply }
+    });
+  };
+
 	render(){
 		return <div>setting up </div>;
 	}
